481406d9dd556ab3695950289d9233c996adada9
Omar Duarte <omarld@hotmail.com>
2020-12-23 07:57:49 -0700

    installing redux core

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import SessionSearch from 'components/SessionSearch';
 
 function App() {
   return (
     <div className="App">
       <h1>QM Coding Challenge</h1>
+      <SessionSearch />
     </div>
   );
 }
---
+++ src/components/SessionSearch.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class SessionSearch extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <h1>Session Search</h1>
+            </div>
+        )
+    }
+}
+
+export default SessionSearch;
